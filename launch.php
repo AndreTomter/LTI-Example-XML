9797bd4d73704c349c26464c3c88b1ab132961bd
AndreTomter <83118639+AndreTomter@users.noreply.github.com>
2023-08-28 11:43:29 +0200

    Update launch.php

--- launch.php
+++ launch.php
@@ -4,9 +4,9 @@
 </head>
 <body><h1>Hello, Library!</h1>
 	
-	<p>Welcome, <?php echo $_POST['lis_person_name_given']; ?></p>
+	<p>Velkommen, <?php echo $_POST['lis_person_name_given']; ?></p>
 	<h2>Tools for <?php echo $_POST['context_title']; ?></h2>
-	<p>42</p>
+	<p>Din brukerid er : <?php echo $_POST['user_id']; ?></p>
 	
 </body>
 </html>
